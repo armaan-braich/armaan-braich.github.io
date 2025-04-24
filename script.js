e638047c129c914799540dcd86bd6a35e5100354
armaan-braich <armaanbraich@hotmail.com>
2025-04-24 01:37:33 -0700

    progress

--- script.js
+++ script.js
@@ -38,10 +38,6 @@ messengerBtn.addEventListener("click", () => {
     window.location.href = "messenger.html";
 });
 
-allProjects.forEach((project) => {
-    project.addEventListener('click', () => expand(project));
-});
-
 dnnBtn.addEventListener("click", () => {
     window.location.href = "ha-dnn.html";
 });
@@ -84,60 +80,3 @@ function copyToClip(type) {
         }, 1500);
     });
 }
-
-
-function fadeOnScroll() {
-    const scrollPosition = window.scrollY;
-    const transY = scrollPosition;
-
-    if (scrollPosition > 100) {
-        skillsSection.style.transform = 'translateX(25%)';
-        skillsSection.style.opacity = '1';
-    }
-    if (scrollPosition > 0) {
-        aboutSection.style.transform = `translateX(25%)`;
-        aboutSection.style.opacity = '1';
-        homeSection.style.transform = `translateX(-75%)`;
-    }
-    else {
-        aboutSection.style.transform = 'translateX(200%)';
-        aboutSection.style.opacity = '0';
-        skillsSection.style.transform = 'translateX(200%)';
-        skillsSection.style.opacity = '0';
-        homeSection.style.transform = 'translateX(0)';
-        homeSection.style.opacity = '1';
-    }
-
-    if (transY < 200) {
-        for (let i = 0; i < fadeSection.length; i++) {
-            bodySection[i].style.transform = `translateY(${transY}px)`;  // Apply the transform individually
-        }
-    }
-}
-
-function expand(project) {
-    const allProjects = document.getElementById("all-projects");
-    const rect = project.getBoundingClientRect();
-    const parentRect = allProjects.getBoundingClientRect();
-
-    // Calculate relative position
-    const xPos = (rect.left - parentRect.left) / parentRect.width;
-    const yPos = (rect.top - parentRect.top) / parentRect.height;
-
-    // Determine transform-origin based on position
-    let originX = xPos < 0.33 ? "0%" : xPos > 0.66 ? "100%" : "50%";
-    let originY = yPos < 0.33 ? "0%" : (yPos < 0.66) ? "50%" : "100%";
-
-    // Apply transform-origin and expand
-    project.style.transformOrigin = `${originX} ${originY}`;
-
-    // Collapse any other expanded projects
-    document.querySelectorAll('.project.expanded').forEach((p) => {
-        if (p !== project) p.classList.remove('expanded');
-    });
-
-    // Toggle expansion
-    project.classList.toggle("expanded");
-}
-
-window.addEventListener('scroll', fadeOnScroll);
