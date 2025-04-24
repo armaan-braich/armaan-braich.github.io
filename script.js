56a510593921a34c71ce77f7583a817c4524880c
Armaan Braich <abraic01@ssh.ece.ubc.ca>
2025-04-24 02:46:20 -0700

    fixed up media queries, should help on diff devices

--- script.js
+++ script.js
@@ -5,52 +5,52 @@ const fadeSection = document.getElementsByClassName('fade');
 const bodySection = document.getElementsByTagName('body');
 const allProjects = document.querySelectorAll('.project');
 
-const chessBtn = document.getElementById("chessBtn");
-const carBtn = document.getElementById("carBtn");
-const tempestBtn = document.getElementById("tempestBtn");
-const crashBtn = document.getElementById("crashBtn");
-const messengerBtn = document.getElementById("messengerBtn");
-const dnnBtn = document.getElementById("dnnBtn");
-const recipeBtn = document.getElementById("recipeBtn");
-const baccaratBtn = document.getElementById("baccaratBtn");
-const osBtn = document.getElementById("osBtn");
+const chess = document.getElementById("chess");
+const car = document.getElementById("car");
+const tempest = document.getElementById("tempest");
+const crash = document.getElementById("crash");
+const messenger = document.getElementById("messenger");
+const dnn = document.getElementById("dnn");
+const recipe = document.getElementById("recipe");
+const baccarat = document.getElementById("baccarat");
+const os161 = document.getElementById("os161");
 
 const contactEmail = document.getElementById("email");
 const contactPhone = document.getElementById("phone");
 
-chessBtn.addEventListener("click", () => {
+chess.addEventListener("click", () => {
     window.location.href = "chess.html";
 });
 
-carBtn.addEventListener("click", () => {
+car.addEventListener("click", () => {
     window.location.href = "car.html";
 });
 
-tempestBtn.addEventListener("click", () => {
+tempest.addEventListener("click", () => {
     window.location.href = "tempest.html";
 });
 
-crashBtn.addEventListener("click", () => {
+crash.addEventListener("click", () => {
     window.location.href = "crash.html";
 });
 
-messengerBtn.addEventListener("click", () => {
+messenger.addEventListener("click", () => {
     window.location.href = "messenger.html";
 });
 
-dnnBtn.addEventListener("click", () => {
+dnn.addEventListener("click", () => {
     window.location.href = "ha-dnn.html";
 });
 
-recipeBtn.addEventListener("click", () => {
+recipe.addEventListener("click", () => {
     window.location.href = "recipe.html";
 });
 
-baccaratBtn.addEventListener("click", () => {
+baccarat.addEventListener("click", () => {
     window.location.href = "baccarat.html";
 });
 
-osBtn.addEventListener("click", () => {
+os161.addEventListener("click", () => {
     window.location.href = "os161.html";
 });
 
@@ -64,16 +64,22 @@ contactPhone.addEventListener("click", () => {
 });
 
 function copyToClip(type) {
+    var element;
+    var text;
+    var originalText;
+    
     if (type === "email") {
         element = contactEmail;
         text = "armaanbraich@hotmail.com";
+        originalText = "Email";
     }
     else {
         element = contactPhone;
         text = "2369994488";
+        originalText = "Phone";
     }
+
     navigator.clipboard.writeText(text).then(() => {
-        const originalText = element.textContent;
         element.textContent = "Copied!";
         setTimeout(() => {
             element.textContent = originalText;
